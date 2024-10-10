a88fd0450e3a3dd73267083b6802c8b805521a1b
Lujjan25 <lujjan@protonmail.com>
2024-10-10 11:25:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var writeup = document.getElementsByTagName("link")[0].import;
+var writeup = document.getElementsByTagName("link")[1].import;
 var absatz = writeup.getElementsByTagName("p")[0];
 window.addEventListener("load", function() {
 document.getElementsByTagName("body")[0].replaceChild(writeup.getElementsByTagName("body")[0], document.getElementsByID("writeup")[0]);
