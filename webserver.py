4eb07f0f828c44c32f2d888a2ce39f7b9c1260af
Lujjan25 <lujjan@protonmail.com>
2024-10-10 13:43:50 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -5,11 +5,11 @@ class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path[-5:] != '.html':
-            self.path = '/py-webserver/index.html'
+            self.path = '/index.html'
         if '..' in self.path:
-            self.path = '/py-webserver/index.html'
-        if self.path == '/py-webserver/':
-            self.path = '/py-webserver/index.html'
+            self.path = '/index.html'
+        if self.path == '/':
+            self.path = '/index.html'
         try:
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
